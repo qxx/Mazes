605cee8247051100c35d04a01fcfa1d13035cb4b
Xi Qian <charlesqx@gmail.com>
2015-02-09 14:16:12 -0500

    organize code parts

--- Maze.rb
+++ Maze.rb
@@ -4,7 +4,6 @@ class Maze
   def initialize(xSize, ySize)
     @xSize = xSize
     @ySize = ySize
-    checkGemVersion()
     load_empty()
   end
 
@@ -69,67 +68,13 @@ class Maze
     plane_display(maze_display)
   end
 
-  def checkGemVersion
-    
-  end
-
-  def buildTree(rootName)
-    @begI = rootName
-    @solveMaze = @maze.clone
-    @solveMaze[rootName] = "2"
-    @root = Tree::TreeNode.new(rootName.to_s)
-
-    addChildren(@root)
-  end
-
-  def addChildren(p)
-    index = p.name.to_i
-    [topOf(index), bottomOf(index), leftOf(index), rightOf(index)].each_with_index do |childName, i|
-      tryAddNode(childName, p)
-    end
-    p.children.each do |child|
-      addChildren(child)
-    end
-    return unless p.has_children?
-  end
-
-  def tryAddNode(childName, parent)
-    return if @solveMaze[childName].to_i > 0
-    @solveMaze[childName] = "2" # 2 => has been visited
-    parent << Tree::TreeNode.new(childName.to_s)
-    return to_xy(childName)
-  end
-
+  private
+  # Part 1 Load maze
   def load_empty(cell = "0")
     @maze = "1" * @xSize + ("1" + cell * (@xSize - 2) + "1") * (@ySize - 2) + "1" * @xSize
   end
 
-  def solved_digit_to_symbol(*args)
-    return @solveMaze[to_index(*args)] == "3" ? "*" : digit_to_symbol(*args)
-  end
-
-  def digit_to_symbol(*args)
-    return " " if @maze[to_index(*args)]=="0"
-    s = getSurroundings(*args)
-    if (s == "1100")
-      return "|"
-    elsif s == "0011"
-      return "-"
-    else
-      return "+"
-    end
-  end
-
-  def digitAt(*args)
-    return args[0].nil? ? nil : @maze[to_index(*args)]
-  end
-
-  def getSurroundings(*args)
-    s = digitAt(topOf(*args)).to_i.to_s + digitAt(bottomOf(*args)).to_i.to_s +
-        digitAt(leftOf(*args)).to_i.to_s + digitAt(rightOf(*args)).to_i.to_s
-    return s
-  end
-
+  # Part 2 Move around
   def to_xy(*args)
     if args.size == 1
       return (args[0] % @xSize) , (args[0] / @xSize)
@@ -164,6 +109,61 @@ class Maze
   def rightOf(*args)
     x, y = to_xy(*args)
     return x == @xSize - 1 ? nil : to_index(x + 1, y)
+  end
+
+  # Part 3 Display maze
+  def solved_digit_to_symbol(*args)
+    return @solveMaze[to_index(*args)] == "3" ? "*" : digit_to_symbol(*args)
+  end
+
+  def digit_to_symbol(*args)
+    return " " if @maze[to_index(*args)]=="0"
+    s = getSurroundings(*args)
+    if (s == "1100")
+      return "|"
+    elsif s == "0011"
+      return "-"
+    else
+      return "+"
+    end
+  end
+
+  def digitAt(*args)
+    return args[0].nil? ? nil : @maze[to_index(*args)]
+  end
+
+  def getSurroundings(*args)
+    s = digitAt(topOf(*args)).to_i.to_s + digitAt(bottomOf(*args)).to_i.to_s +
+        digitAt(leftOf(*args)).to_i.to_s + digitAt(rightOf(*args)).to_i.to_s
+    return s
+  end
+
+  # Part 4 Solve maze
+  def buildTree(rootName)
+    @begI = rootName
+    @solveMaze = @maze.clone
+    @solveMaze[rootName] = "2"
+    @root = Tree::TreeNode.new(rootName.to_s)
+
+    addChildren(@root)
+  end
+
+  def addChildren(p)
+    index = p.name.to_i
+    [topOf(index), bottomOf(index), leftOf(index), rightOf(index)].each_with_index do |childName, i|
+      tryAddNode(childName, p)
+    end
+    p.children.each do |child|
+      addChildren(child)
+    end
+    return unless p.has_children?
+  end
+
+  def tryAddNode(childName, parent)
+    return if @solveMaze[childName].to_i > 0
+    @solveMaze[childName] = "2" # 2 => has been visited
+    parent << Tree::TreeNode.new(childName.to_s)
+    return to_xy(childName)
   end
 
 end
